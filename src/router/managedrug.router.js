58603ee0e5906335c613e8f64957fd685289d907
woraphat12T <137477332+woraphat12T@users.noreply.github.com>
2023-09-03 23:12:01 +0700

    Update managedrug.router.js

--- src/router/managedrug.router.js
+++ src/router/managedrug.router.js
@@ -3,7 +3,7 @@ export default {
     component: () => import('../views/manageDrug/layout.vue'),
     redirect: '/manageDrug/dashboard',
     children: [
-        { path: '/manageDrug/dashboard', component: () => import('/views/manageDrug/dashboard.vue') },
+        { path: '/manageDrug/dashboard', component: () => import('../views/manageDrug/dashboard.vue') },
         { path: '/manageDrug/addDrug', component: () => import('../views/manageDrug/addDrug.vue') },
         { path: '/manageDrug/manageDrug', component: () => import('../views/manageDrug/manageDrug.vue') },
         { path: '/manageDrug/pickupDrug', component: () => import('../views/manageDrug/pickupDrug.vue') },
